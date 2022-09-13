2274b2dd366ecea6460c2fb0d4a35236e120107a
Ruffshok <ruffshok@yahoo.com>
2022-09-12 23:24:18 -0700

    type out the alphabet in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+
+int main()
+
+{
+
+	   int a;
+
+	      for(a = 'a'; a <= 'z'; a++)
+
+		          {
+
+				          putchar(a);
+
+					      }
+
+	          putchar('\n');
+
+		      return 0;
+
+}
