3b3c4cbf1248e15f3b8d79e9b483937de490a853
Ruffshok <111015150+Ruffshok@users.noreply.github.com>
2022-09-14 06:31:30 +0100

    created main.h file with all main functions

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,64 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+void positive_or_negative(int i);
+int main(void);
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+
+/**
+* main - prints the largest of 3 integers
+* Return: 0
+*/
+
+int main(void)
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+
+/**
+* main - takes a date and prints how many days are left in the year, taking
+* leap years into account
+* Return: 0
+*/
+
+int main(void)
+
+/**
+* convert_day - converts day of month to day of year, without accounting
+* for leap year
+* @month: month in number format
+* @day: day of month
+* Return: day of year
+*/
+
+int convert_day(int month, int day)
+  
+/**
+* print_remaining_days - takes a date and prints how many days are
+* left in the year, taking leap years into account
+* @month: month in number format
+* @day: day of month
+* @year: year
+* Return: void
+*/
+
+void print_remaining_days(int month, int day, int year)
+
+#endif
