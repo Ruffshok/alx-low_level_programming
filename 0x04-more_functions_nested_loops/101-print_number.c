7013f356083e7dc9ef0af6956bf04b06cf8a1c25
Ruffshok <111015150+Ruffshok@users.noreply.github.com>
2022-09-16 00:19:37 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,37 +1,35 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
- * print_number - prints numbers
+ * print_number - print an integer, without using long, arrays, or pointers
  * @n: number to be printed
- * Return:void
  */
 
 void print_number(int n)
 {
+	unsigned int tens, digit, positive = n;
+	double t_beg = 1;
 
-	int d = 1, i = 0, ii = 0;
-
-	if (n < 0)
+	if (n == 0)
+		_putchar('0');
+	else
 	{
-		_putchar('-');
-		n = -n;
+		if (n < 0)
+		{
+			positive = n * -1;
+			_putchar('-');
+		}
+
+		while (t_beg <= positive)
+			t_beg *= 10;
+		tens = t_beg / 10;
+
+		while (tens >= 1)
+		{
+			digit = positive / tens;
+			_putchar(digit + '0');
+			positive = (positive - (tens * digit));
+			tens /= 10;
+		}
 	}
-
-	while (n / d != 0)
-	{
-		d *= 10;
-		i++;
-	}
-	d = d / 10;
-
-	while (ii < i)
-	{
-		_putchar('0' + n / d);
-		n = n - (n / d) * d;
-		d = d / 10;
-		ii++;
-	}
-
-	if (i == 0)
-		_putchar('0' + n);
-
 }
