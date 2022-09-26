5a7432833d86177881737881e2515c8c36974b42
Ruffshok <111015150+Ruffshok@users.noreply.github.com>
2022-09-26 20:09:13 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,35 +1,22 @@
 #include "main.h"
-#define NULL 0
-
 /**
- * _strchr - locate 1st occurrence of char in string and returns pointer there
- * @s: string to search
- * @c: target characer
- * Return: pointer to that character in string
+ * _strchr - locates character in string
+ * @s: string to locate char
+ * @c: character to find
+ * Return: character value
  */
-
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+	int i;
 
-	while (s[i] != '\0' && s[i] != c) /* find match */
-		i++;
-
-	if (s[i] == c) /* if match, assign to address */
-		return (&s[i]);
-	else
-		return (NULL);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	if (s[i] == c)
+		return (s + i);
+	return (0);
 }
-
-/* pointer arithmetic version
-char *_strchr(char *s, char c)
-{
-        int i = 0;
-	while (*(s + i) && *(s + i) != c)
-	    i++;
-	if (*(s + i) == c)
-	    return (s + i);
-	else
-	    return (NULL);
-}
-*/
