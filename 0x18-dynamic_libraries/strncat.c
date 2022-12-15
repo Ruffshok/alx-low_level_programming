02d628098acf9c0b2fff9f2247a9eb22100d736d
Ruffshok <111015150+Ruffshok@users.noreply.github.com>
2022-12-15 08:01:10 +0100

    Update strncat.c

--- 0x18-dynamic_libraries/strncat.c
+++ 0x18-dynamic_libraries/strncat.c
@@ -1,27 +1,28 @@
 #include "main.h"
+
 /**
- * _strncat - concats two arrays
- *
- * @dest: destination of concat
- * @src: source array to concat
- * @n: amount of times to append
- *
- * Return: char value
+ * _strncat - concatenate n bytes to destination string
+ * @dest: string to be appended to
+ * @src: string to append
+ * @n: append n number of bytes(chars)
+ * Return: concatenated string
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int j;
+	int i = 0;
+	int j = 0;
 
-	for (j = 0; dest[j] != '\0'; j++)
-	{}
+	while (dest[i] != '\0') /*i is index of null terminator*/
+		i++;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	while (src[j] != src[n]) /*append replacing null terminator*/
 	{
-		dest[j + i] = src[i];
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
-	dest[j + i] = '\0';
+	dest[i] = '\0';
 
 	return (dest);
-
 }
